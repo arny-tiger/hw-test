5c0208905f852de87e0f83898cc638cd08350823
Semyon Ancherbak <semyon.ancherbak@gmail.com>
2023-11-28 23:23:03 +0400

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,28 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := make(Bi)
+
+	go func() {
+		<-done
+		close(out)
+	}()
+
+	go func() {
+		stageOut := in
+		for _, stage := range stages {
+			stageOut = stage(stageOut)
+		}
+		for v := range stageOut {
+			select {
+			case <-done:
+				return
+			default:
+				out <- v
+			}
+		}
+		close(out)
+	}()
+
+	return out
 }
